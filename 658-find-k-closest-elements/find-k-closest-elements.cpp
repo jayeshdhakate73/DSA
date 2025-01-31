9c45eb2b08c2cfcaa5b98461659bde6b27953027
Jayesh Dhakate <jayeshdhakate73@gmail.com>
2025-01-31 23:47:54 +0530

    Time: 0 ms (100.00%) | Memory: 36.3 MB (65.38%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<pair<int, int>> diffs;  // Store {absolute difference, element}
-        
-        for (int i = 0; i < arr.size(); i++) {
-            diffs.push_back({abs(arr[i] - x), arr[i]}); 
-            //We iterate through each element in arr and calculate the absolute difference between the element arr[i] and x. For each element, we push a pair {difference, element} into the diffs vector. For example, if arr = [1, 3, 5, 7] and x = 4: The differences would be {3, 1}, {1, 3}, {1, 5}, and {3, 7}
-        }
-        
-        // Sort based on absolute difference, if same, sort by element value
-        sort(diffs.begin(), diffs.end());
 
-        vector<int> result;
-        for (int i = 0; i < k; i++) {
-            result.push_back(diffs[i].second);
+    vector<int> twoPointer(vector<int>& arr, int k, int x) {
+        int l=0;
+        int h=arr.size()-1;
+        while(h-l>=k){
+            if(x-arr[l] > arr[h]-x){
+                l++;
+            }
+            else{
+                h--;
+            }
+             
         }
-        
-        sort(result.begin(), result.end()); // Ensure result is sorted
-        return result;
+        vector<int> ans;
+        for(int i=l; i<=h; i++){
+            ans.push_back(arr[i]);
+        }
+        return ans;
+
+    }
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        return twoPointer(arr, k, x); 
     }
 };
+
