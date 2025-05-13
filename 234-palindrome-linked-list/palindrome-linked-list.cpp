b73dbf5dd7ba5a57d9e074c5789a333bad74e149
Jayesh Dhakate <jayeshdhakate73@gmail.com>
2025-05-13 18:07:06 +0530

    Time: 2 ms (66.50%) | Memory: 118.1 MB (70.02%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,63 +10,79 @@
  */
 class Solution {
 public:
-    ListNode* getMiddle(ListNode* &head, ListNode* &middleNodeKaPrev) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast != NULL) {
-            fast = fast->next;
-            if (fast != NULL) {
-                fast = fast->next;
+
+    ListNode* getMiddle(ListNode* &head, ListNode* &middleNodeKaPrev){
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL){
+            fast=fast->next;
+             if(fast!=NULL){
+                fast=fast->next;
                 middleNodeKaPrev = slow;
-                slow = slow->next;
-            }
+                slow=slow->next;
+             }
         }
         return slow;
     }
 
-    ListNode* Reverse(ListNode* &prev, ListNode* &curr) {
-        while (curr != NULL) {
+    ListNode* Reverse(ListNode* &prev, ListNode* &curr){
+        while(curr!=NULL){
             ListNode* forward = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = forward;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
         }
         return prev;
     }
 
     bool isPalindrome(ListNode* head) {
-        if (head == NULL) return true;
-        if (head->next == NULL) return true;
 
-        // Get middle node and the node before it
+        if(head == NULL){
+            //LL is empty
+            return true;
+        }
+        if(head->next==NULL){
+            //single node
+            return true;
+        }
+
+        //traverse till middle node and break LL in 2
         ListNode* firstHalfHead = head;
         ListNode* middleNodeKaPrev = NULL;
         ListNode* middleNode = getMiddle(head, middleNodeKaPrev);
-
-        // Break the list into two halves
+        
+        //break
         if (middleNodeKaPrev != NULL) {
             middleNodeKaPrev->next = NULL;
         } else {
-            // For 2-node case, break at head
+            //for 2 node case, break from head
             head = NULL;
         }
 
-        // Reverse second half
+        //reverse 2nd half of LL
         ListNode* prev = NULL;
         ListNode* curr = middleNode;
-        ListNode* secondHalfHead = Reverse(prev, curr);
+        ListNode* secondHalfHead = Reverse(prev,curr);
 
-        // Compare both halves
+        //compare both the halves and decide T/F
+        //even length waale case mai dono half ki length euqal hogi
+        //odd waale case mai 2nd half ki len badi hogi by 1
+        //thats whu comparison 1st half ke hisab se hoga
         ListNode* temphead1 = firstHalfHead;
         ListNode* temphead2 = secondHalfHead;
-        while (temphead1 != NULL) {
-            if (temphead1->val != temphead2->val) {
+        while(temphead1 != NULL){
+            if(temphead1->val != temphead2->val){
+                //not a palindrome
                 return false;
             }
-            temphead1 = temphead1->next;
-            temphead2 = temphead2->next;
+            else{
+                //data equal hai toh dono ko aage badhao
+                temphead1 = temphead1->next;
+                temphead2 = temphead2->next;
+            }
         }
-
+        //yaha tak pahocha mtlb palindrome hai 
         return true;
+        
     }
 };
