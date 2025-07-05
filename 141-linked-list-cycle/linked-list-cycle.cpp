5281154ccd12917ca9a1cb4c9de69ad6fee8a23f
Jayesh Dhakate <jayeshdhakate73@gmail.com>
2025-07-05 12:51:12 +0530

    Time: 8 ms (81.18%) | Memory: 11.8 MB (53.18%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,46 +9,18 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* slow= head;
         ListNode* fast=head;
+        ListNode* slow=head;
         while(fast!=NULL){
             fast=fast->next;
             if(fast!=NULL){
                 fast=fast->next;
-                slow=slow->next; 
-                if(fast==slow){
-                    //at some point fast=slow if there is a cycle  
-                    //cycle present hai 
-                    return true;
-                }
+                slow=slow->next;
             }
-
+             if(fast==slow){
+                return true;
+            }
         }
-        return false;
-
-
-
-
-        //METHOD 1 using map
-        // unordered_map<ListNode* , bool> m;
-        // ListNode* temp = head;
-
-        // while(temp!=NULL){
-        //     if(m[temp]==true){
-        //         //pahale se he true hai 
-        //         //cycle present hai 
-        //         return true;
-        //     }
-        //     else{
-        //         //pahale se true nhi hai mtlb false hai 
-        //         //true krna pdega 
-        //         m[temp]=true;
-        //     }
-        //     temp = temp->next;
-        // }
-        // //puri LL traverse ho chuki hai 
-        // return false; 
-
-        
+       return false;
     }
 };
