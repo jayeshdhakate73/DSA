3cb2f9c5a4d5249ccdc1c617d5a181ff8475324f
Jayesh Dhakate <jayeshdhakate73@gmail.com>
2025-08-05 23:18:14 +0530

    Time: 1 ms (46.13%) | Memory: 112.5 MB (12.14%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -5,15 +5,30 @@ public:
         int deficit=0;
         int balance=0;
 
-        for(int i=0; i<gas.size(); i++){
-            balance += gas[i] - cost[i];
-            //+ve balance -> nothing to worry about 
-            //-ve balance -> ye deficit ko darshata hai 
-            if(balance<0){
-                deficit += abs(balance);
-                start = i+1;
-                balance =0;
+        // for(int i=0; i<gas.size(); i++){
+        //     balance += gas[i] - cost[i];
+        //     //+ve balance -> nothing to worry about 
+        //     //-ve balance -> ye deficit ko darshata hai 
+        //     if(balance<0){
+        //         deficit += abs(balance);
+        //         start = i+1;
+        //         balance =0;
 
+        //     }
+        // }
+        // if(balance - deficit >=0){
+        //     return start;
+        // }
+        // return -1;
+
+        for(int i=0; i<gas.size(); i++){
+            if(balance + gas[i] < cost[i]){
+                deficit += abs(balance + gas[i] - cost[i]);
+                start =i+1;
+                balance =0;
+            }
+            else{
+                balance += gas[i] - cost[i];
             }
         }
         if(balance - deficit >=0){
